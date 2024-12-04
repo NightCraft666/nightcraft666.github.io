9e59b2788f4d436e44a87dc95fce1c936b51c90a
NightCraft <susplacek@gmail.com>
2024-12-04 19:12:10 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,10 @@
-
+let clicks = 0;
+document.getElementById("EasterEgg").onclick = function() {
+  clicks += 1
+  if(clicks == 3) {
+    const kod = prompt("Wpisz swój kod:");
+    if(kod == 2222) {
+      window.location.href = "../index.html";
+    }
+  }
+}
