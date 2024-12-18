0443a8564890ae1ea7e8c1622093386a9d012b0a
NightCraft <susplacek@gmail.com>
2024-12-18 19:44:10 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,25 @@
 let clicks = 0;
-document.getElementById("EasterEgg").onclick = function() {
-  clicks += 1
-  if(clicks == 3) {
-    const kod = prompt("Wpisz swój kod:");
-    if(kod == 2222) {
-      window.location.href = "../cookies.html";
+if(window.location.href == "../index.html") {
+  document.getElementById("EasterEgg").onclick = function() {
+    clicks += 1
+    if(clicks == 5) {
+      const kod = prompt("Wpisz swój kod:");
+      if(kod == 2222) {
+        window.location.href = "../cookies.html";
+      }
     }
   }
+}
+
+document.getElementById("tt").onclick = function() {
+  window.location.href = "https://www.tiktok.com/@nightcraft2115?lang=pl-PL";
+}
+document.getElementById("dc").onclick = function() {
+  window.location.href = "https://discord.gg/uWnsGDymhF";
+}
+document.getElementById("git").onclick = function() {
+  window.location.href = "https://github.com/NightCraft666";
+}
+document.getElementById("yt").onclick = function() {
+  window.location.href = "https://www.youtube.com/@nightcraft2137";
 }
